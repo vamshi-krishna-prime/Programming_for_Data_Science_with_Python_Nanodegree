d0f9a37a34039c2009711ef04e16d7b84333ae0a
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:54:39 +0530

    SQL > Basics > Added Query 13

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/13-where-non-numeric-2.sql
@@ -0,0 +1,11 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Filter the accounts table to include the company name, website, and the
+primary point of contact (primary_poc) just for the 'Exxon Mobil' company in the
+accounts table.
+*/
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
