36945b3430c4be980cf85491b5afeb8c014b8635
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:54:01 +0530

    SQL > Basics > Added Query 12

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/12-where-non-numeric-1.sql
@@ -0,0 +1,20 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Pull all the data related to the company named 'United Technologies' in the
+accounts table
+*/
+
+SELECT *
+FROM accounts
+WHERE name = 'United Technologies';
+
+/*
+The WHERE statement can also be used with non-numeric data. We can use
+the '=' and '!=' operators here. You need to be sure to use single quotes (just
+be careful if you have quotes in the original text) with the text data,
+not double quotes.
+
+Commonly when we are using WHERE with non-numeric data fields, we use
+the 'LIKE', 'NOT', or 'IN' operators.
+*/
