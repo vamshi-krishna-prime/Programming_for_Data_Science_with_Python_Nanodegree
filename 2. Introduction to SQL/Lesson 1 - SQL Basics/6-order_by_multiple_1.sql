5c6ebb20c9e32d69af0dfd8809b51b31f3ed563b
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:49:23 +0530

    SQL > Basics > Added Query 6

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/6-order_by_multiple_1.sql
@@ -0,0 +1,17 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+Write a query to return the highest to lowest total_amt_usd with respect to
+account_id s sorted lowest to highest.
+Include the account_id, and total_amt_usd.
+
+In short the query should return the account_id s sorted from lowest to highest
+and internally showing the order with highest total_amt_usd first inside the id.
+*/
+
+SELECT account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
+/*
+This is not same as:
+ORDER BY total_amt_usd DESC, account_id
+*/
