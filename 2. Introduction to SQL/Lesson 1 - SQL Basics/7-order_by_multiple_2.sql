907846b6e5c4417cf426ffce8d8034d53928763c
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:49:59 +0530

    SQL > Basics > Added Query 7

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/7-order_by_multiple_2.sql
@@ -0,0 +1,17 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Write a query that displays the order ID, account ID, and total dollar amount
+for all the orders, sorted first by the account ID (in ascending order), and
+then by the total dollar amount (in descending order).
+*/
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
+
+/*
+In above query all of the orders for each account ID are grouped together, and
+then within each of those groupings, the orders appear from the greatest order
+amount to the least.
+*/
