2b53c8b24370d9e62421bbda73096e651912752d
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:48:01 +0530

    SQL > Basics > Added Query 4

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/4-order_by_3.sql
@@ -0,0 +1,10 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+Write a query to return the top 5 orders in terms of largest total_amt_usd.
+Include the id, account_id, and total_amt_usd.
+*/
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
