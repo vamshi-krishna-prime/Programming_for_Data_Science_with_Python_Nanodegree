1fba475fa75499a90fa30ee6bc9200c9e502f0f4
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-26 23:47:17 +0530

    SQL > Basics > Added Query 3

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/3-order_by_2.sql
@@ -0,0 +1,10 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+Write a query to return the 10 earliest orders in the orders table.
+Include the id, occurred_at, and total_amt_usd.
+*/
+
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
