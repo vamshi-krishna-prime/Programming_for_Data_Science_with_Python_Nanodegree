eb9d8c3c0befb7d74ebc86bcee7096f072d88fdf
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-27 23:31:26 +0530

    SQL > Basics > Added Query 23

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/23-in-companies.sql
@@ -0,0 +1,11 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Use the 'accounts' table to find the account 'name', 'primary_poc', and
+'sales_rep_id' for 'Walmart', 'Target', and 'Nordstrom'.
+*/
+
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
