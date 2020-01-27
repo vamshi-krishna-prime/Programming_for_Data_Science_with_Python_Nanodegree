5fd6821b3227a3a14f505df65d1f3066288c3399
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-27 23:46:23 +0530

    SQL > Basics > Added Query 40

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/40-or-either-paper-qty-greater-than-4000.sql
@@ -0,0 +1,11 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Find list of orders ids where either gloss_qty or poster_qty is greater than
+4000. Only include the id field in the resulting table.
+*/
+
+
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000;
