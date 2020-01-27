e8f740a42de8432d60e2c4c5359888163c4860ae
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-27 23:33:05 +0530

    SQL > Basics > Added Query 25

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/25-not-like-google_url-web_events.sql
@@ -0,0 +1,15 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+
+Imagine yourself as the 'Digital Marketing Manager' at 'Parch and Posey'
+company. Identify the web traffic that came other than any 'Google' page.
+Also display, id, account_id, occurred_at and channel columns in the
+'web_events' table.
+
+Hint: links are listed under 'referrer_url' column in the 'web_events' table.
+*/
+
+
+SELECT id, account_id, occurred_at, channel, referrer_url
+FROM web_events
+WHERE referrer_url NOT LIKE '%google%';
