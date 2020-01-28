2a99b481f2282cbb0450f1a8293ea4889a02f9cd
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-28 20:20:11 +0530

    SQL > Basics > Modified Query 11

--- 2. Introduction to SQL/Lesson 1 - SQL Basics/11-where-less-than.sql
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/11-where-less-than.sql
@@ -1,5 +1,7 @@
 /*
-SQL practice in Udacity's Business Analytics Nanodegree Program
+Programming for Data Science with Python Nanodegree
+Udacity's certified program
+SQL practice > Lesson 1 : SQL Basics
 
 Pulls the first 10 rows and all columns from the orders table that have a
 total_amt_usd less than 500.
