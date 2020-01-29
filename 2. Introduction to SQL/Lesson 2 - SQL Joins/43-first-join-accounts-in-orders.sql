e99d42feba395587253e6c7106dbb2782f2e5ebb
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-29 23:37:55 +0530

    SQL > Joins > Modified Query 43

--- 2. Introduction to SQL/Lesson 2 - SQL Joins/43-first-join-accounts-in-orders.sql
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/43-first-join-accounts-in-orders.sql
@@ -1,5 +1,7 @@
 /*
-SQL practice in Udacity's Business Analytics Nanodegree Program
+Programming for Data Science with Python Nanodegree
+Udacity's certified program
+SQL practice > Lesson 1 : SQL Basics
 
 First JOIN
 JOIN / merge 'id' column in the accounts table with 'account_id' column in the
