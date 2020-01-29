49d40d4441f1557439e112ff168416abc4d6a8e2
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-29 23:42:21 +0530

    SQL > Joins > Added Query 47

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/47-join-orders-accounts-show-name-and-occurred_at.sql
@@ -0,0 +1,14 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program
+SQL practice > Lesson 1 : SQL Basics
+
+Try pulling 'name' from the 'accounts' table, and 'occurred_at' from
+the 'orders' table.
+*/
+
+
+SELECT accounts.name, orders.occurred_at
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
