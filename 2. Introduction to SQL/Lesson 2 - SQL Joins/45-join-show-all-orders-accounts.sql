e5dd4f5f303d54d01e789802861b70bcd5f25afe
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-29 23:39:21 +0530

    SQL > Joins > Modified Query 45

--- 2. Introduction to SQL/Lesson 2 - SQL Joins/45-join-show-all-orders-accounts.sql
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/45-join-show-all-orders-accounts.sql
@@ -1,5 +1,7 @@
 /*
-SQL practice in Udacity's Business Analytics Nanodegree Program
+Programming for Data Science with Python Nanodegree
+Udacity's certified program
+SQL practice > Lesson 1 : SQL Basics
 
 Try pulling all the data from the 'accounts' table, and all the data from
 the 'orders' table.
