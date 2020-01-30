0402945458e6f759bc754c53e471a31adf4ae753
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-30 23:37:30 +0530

    SQL > Joins > Added Query 56

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/56-left_join-alias-country-state.sql
@@ -0,0 +1,15 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 2 : SQL Joins
+
+Make a LEFT JOIN to the 'Country' and 'State' table provided in the
+SQL section, lesson 2, section 17.
+Display 'countryid', 'countryName' and 'stateName'
+*/
+
+
+
+SEELECT c.countryid, c.countryName, s.stateName
+FROM Country c
+LEFT JOIN State s
+ON c.countryid = s.countryid;
