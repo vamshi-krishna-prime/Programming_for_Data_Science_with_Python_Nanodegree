833b4c13ddb2bbcbc91d5f0484fc1fd66fe9e257
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-30 23:51:03 +0530

    SQL > Aggregations > Added Query 66

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/66-count-accounts-id.sql
@@ -0,0 +1,11 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 3 : SQL Aggregations
+
+find the count of id's in the accounts table.
+*/
+
+
+
+SELECT COUNT(a.id) id_count
+FROM accounts a;
