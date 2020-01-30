d77d1acba8703551fc8d74b5a488ce66dde8a727
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-30 23:53:53 +0530

    SQL > Aggregations > Added Query 71

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/71-aggregation-q4-orders-total-standard-gloss.sql
@@ -0,0 +1,13 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 3 : SQL Aggregations
+
+Find the total amount spent on 'standard_amt_usd' and 'gloss_amt_usd' paper
+for each order in the 'orders' table. This should give a dollar amount for
+each order in the table.
+*/
+
+
+
+SELECT (o.standard_amt_usd + o.gloss_amt_usd) AS total_standard_gloss_amt
+FROM orders o;
