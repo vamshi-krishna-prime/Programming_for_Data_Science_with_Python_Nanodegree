a640d7ad821eea7be200489f7d29e7b134e68fd6
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-01-30 23:38:11 +0530

    SQL > Joins > Added Query 57

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/57-join-alias-q1-region-sales_rep-accounts.sql
@@ -0,0 +1,20 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 2 : SQL Joins
+
+Provide a table that provides the region for each sales_rep along with their
+associated accounts. This time only for the Midwest region. Your final table
+should include three columns: the region name, the sales rep name, and the
+account name. Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
+
+
+SELECT r.name region, s.name sales_rep, a.name account
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest'
+ORDER BY a.name;
