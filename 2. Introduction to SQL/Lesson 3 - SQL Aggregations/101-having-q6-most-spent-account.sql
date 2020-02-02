b66c884d44b4c4a989a5401f85e3819da7ead8b5
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-02 22:21:57 +0530

    SQL > Aggregations > Added Query 101

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/101-having-q6-most-spent-account.sql
@@ -0,0 +1,22 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 3 : SQL Aggregations
+
+Which account has spent the most with us?
+*/
+
+
+
+SELECT a.id id, a.name company, SUM(o.total_amt_usd) total_usd
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_usd DESC
+LIMIT 1;
+
+/*
+Answer:
+id	  company	        total_usd
+4211	EOG Resources	  382873.30
+*/
