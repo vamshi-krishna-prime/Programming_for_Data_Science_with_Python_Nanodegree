209535c152bf1e90ca61cc443ca68f848f55b3ee
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-05 23:51:42 +0530

    SQL > Data Cleaning > Added Query 167

---
+++ 2. Introduction to SQL/Lesson 5 - SQL Data Cleaning/167-position-q2-first-and-lastname-columns-for-sales_reps-name.sql
@@ -0,0 +1,31 @@
+/*
+Programming for Data Science with Python Nanodegree
+Udacity's certified program > SQL practice > Lesson 5 : SQL Data cleaning
+
+Quizzes POSITION & STRPOS:
+You will need to use what you have learned about LEFT & RIGHT, as well as what
+you know about POSITION or STRPOS to do the following quizzes.
+
+Question 2:
+Now see if you can do the same thing (create first and last name columns) for
+every rep name in the sales_reps table. Again provide first and last name
+columns.
+*/
+
+SELECT LEFT(s.name, STRPOS(s.name, ' ') - 1) AS firstname,
+       RIGHT(s.name, (LENGTH(s.name) - POSITION(' ' IN s.name))) AS lastname
+FROM sales_reps s;
+
+/*
+Output 50 results:
+
+firstname	     lastname
+Samuel	       Racine
+Eugena	       Esser
+Michel	       Averette
+Renetta	       Carew
+Cara	         Clarke
+.
+.
+and 45 results more
+*/
