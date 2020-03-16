dece0e20c40542930bfcf3c185674eeee1a98f47
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-03-16 22:56:43 +0530

    Python > Project Bikeshare > Added filter function

--- Project 2 - Explore US Bikeshare Data/bikeshare.py
+++ Project 2 - Explore US Bikeshare Data/bikeshare.py
@@ -7,6 +7,29 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+
+
+    # get user input for month (all, january, february, ... , june)
+
+
+    # get user input for day of week (all, monday, tuesday, ... sunday)
+
+
+    print('-'*40)
+    return city, month, day
+
+
 def main():
     while True:
         city, month, day = get_filters()
