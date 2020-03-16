6541c21a76eafde9e5fe6eb13d6a02146cbda47e
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-03-16 23:07:19 +0530

    Python > Project Bikeshare > Added trip_duration

--- Project 2 - Explore US Bikeshare Data/bikeshare.py
+++ Project 2 - Explore US Bikeshare Data/bikeshare.py
@@ -84,6 +84,22 @@ def station_stats(df):
     print('-'*40)
 
 
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # display total travel time
+
+
+    # display mean travel time
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -91,6 +107,7 @@ def main():
 
         time_stats(df)
         station_stats(df)
+        trip_duration_stats(df)
 
 
 if __name__ == "__main__":
