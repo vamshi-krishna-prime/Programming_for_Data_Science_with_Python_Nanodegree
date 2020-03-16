7c1b5e2f4acfc61a0deb424e5b57e092a58273f8
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-03-16 23:20:46 +0530

    Python > Project Bikeshare > load filterd by day

--- Project 2 - Explore US Bikeshare Data/bikeshare.py
+++ Project 2 - Explore US Bikeshare Data/bikeshare.py
@@ -76,6 +76,12 @@ def load_data(city, month, day):
         df = df[df['month'] == month]
 
 
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df['day_of_week'] == day.title()]
+
+
     return df
 
 
