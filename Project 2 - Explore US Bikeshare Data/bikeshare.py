89d23f6e1ec80e85276f2555504b6e48a15c5c77
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-03-16 23:02:51 +0530

    Python > Project Bikeshare > Added load_data funct

--- Project 2 - Explore US Bikeshare Data/bikeshare.py
+++ Project 2 - Explore US Bikeshare Data/bikeshare.py
@@ -30,9 +30,26 @@ def get_filters():
     return city, month, day
 
 
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+
+
+    return df
+
+
 def main():
     while True:
         city, month, day = get_filters()
+        df = load_data(city, month, day)
 
 
 if __name__ == "__main__":
