85b016501ea07cf858589481e1866c6998ed883c
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-03-16 23:10:03 +0530

    Python > Project Bikeshare > Added script mainloop

--- Project 2 - Explore US Bikeshare Data/bikeshare.py
+++ Project 2 - Explore US Bikeshare Data/bikeshare.py
@@ -129,6 +129,10 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
 
 if __name__ == "__main__":
 	main()
